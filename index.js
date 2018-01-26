7a8917743be7473e46158a23adaef7c41b1ac327
Daniel Gonçalves Machado <daniel@heb.bauru.unesp.br>
2018-01-26 15:22:25 -0200

    Conexão com banco

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('home')
+    res.render('home')
 })
+
+const mysql = require('mysql2/promise')
+const init = async() => {
+    const conn = await mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'futiba'
+    })
+    const [rows, fields] = await conn.execute('select * from users')
+    console.log(rows)
+}
+init()
 
 app.listen(3000, err => {
     if(err)
